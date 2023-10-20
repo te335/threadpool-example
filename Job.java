99ee9e95089a3646a626cef8fbdabb82b1d02991
T <te335@exeter.ac.uk>
2023-10-20 10:11:00 +0100

    commit message?

--- Job.java
+++ Job.java
@@ -7,13 +7,12 @@ public class Job implements Runnable {
 
   public void run () {
     // Undertake required work, here we will emulate it by sleeping for a period
-    System.out.println ("Job: " + jobNumber + " is being processed by thread : "
-    Thread.currentThread ().getName());
-      try {
-        Thread.sleep((int)(1000));
-      } catch (InterruptedException e) {
+    System.out.println ("Job: " + jobNumber + " is being processed by thread : " + Thread.currentThread ().getName());
+    try {
+      Thread.sleep((int)(1000));
+    } catch (InterruptedException e) {
       // no catching as example should not experience interruptions
-      }
-      System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
-     }
+    }
+    System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
+  }
 }
